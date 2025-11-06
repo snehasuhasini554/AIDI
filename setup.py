b7be489c5bffbc1ab98d8f0f3d8eef8fb806a93b
Sneha Suhasini <162284718+snehasuhasini554@users.noreply.github.com>
2025-11-06 18:11:35 +0530

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='aidi_companion',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],  # List your dependencies here
+)
